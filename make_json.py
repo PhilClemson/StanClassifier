7c69f06f98fe5820cbda3bd28e1117ac1ba41f6c
PhilClemson <p.clemson@liverpool.ac.uk>
2021-06-16 16:48:03 +0100

    Fixed whitespace issue so that the code can now run on Windows

--- make_json.py
+++ make_json.py
@@ -38,15 +38,15 @@ def remove_whitespace(text_string,reverse):
                     break
                 else:
                     ind -= 1
-	    if (ind != len_string-1):
-	        ind += 1
-	    start_ind = ind
-	    while start_ind > 0:
-	        if((text_string[ind] == ' ') or (text_string[ind] == '"')):
-	            break
-	        else:
-	            start_ind -= 1
-	    start_ind += 1
+        if (ind != len_string-1):
+            ind += 1
+        start_ind = ind
+        while start_ind > 0:
+            if((text_string[ind] == ' ') or (text_string[ind] == '"')):
+                break
+            else:
+                start_ind -= 1
+        start_ind += 1
         if(ind == start_ind):
             return_string = -1
         else:
@@ -73,31 +73,31 @@ def find_name(text_string):
     size_term = False
     name_end = False
     while ind > 0:
-	if (name_start == True):
-	    if (text_string[ind] != ' '):
-		return_string = text_string[ind] + return_string
-		ind -= 1
-	    else:
-		name_end = True
-	elif (size_term == True):
-	    if (text_string[ind] == '['):
-		return_string = ''
-		size_term = False
-		ind -= 1
-	    else:
-		ind -= 1
-	elif (text_string[ind] == ']'):
-	    size_term = True
-	    ind -= 1
-	elif (text_string[ind] != ';'):
-	    if (text_string[ind] != ' '):
-		name_start = True
-	    else:
-		ind -= 1
-	else:
-	    ind -= 1
-	if (name_end == True):
-	     break
+        if (name_start == True):
+            if (text_string[ind] != ' '):
+                return_string = text_string[ind] + return_string
+                ind -= 1
+            else:
+                name_end = True
+        elif (size_term == True):
+            if (text_string[ind] == '['):
+                return_string = ''
+                size_term = False
+                ind -= 1
+            else:
+                ind -= 1
+        elif (text_string[ind] == ']'):
+            size_term = True
+            ind -= 1
+        elif (text_string[ind] != ';'):
+            if (text_string[ind] != ' '):
+                name_start = True
+            else:
+                ind -= 1
+        else:
+            ind -= 1
+        if (name_end == True):
+             break
     return_string = return_string
         
     return return_string
@@ -155,10 +155,10 @@ def stan_read(text_line_raw,v_type,v_size,v_name,v_bounds,v_num):
     # first remove any comments
     ind = text_line_raw.find('//')
     if (ind == -1):
-	ind = text_line_raw.find('\n')
-	text_line = text_line_raw[:ind-1]
+        ind = text_line_raw.find('\n')
+        text_line = text_line_raw[:ind-1]
     else:
-	text_line = text_line_raw[:ind-1]
+        text_line = text_line_raw[:ind-1]
     if ('int' in text_line):
         v_type.append('int')
         if ('[' in text_line):
@@ -275,7 +275,7 @@ def stan_read(text_line_raw,v_type,v_size,v_name,v_bounds,v_num):
         v_bounds.append(bounds)
         v_num += 1
     elif ('matrix' in text_line):
-	v_type.append('matrix')
+        v_type.append('matrix')
         if ('[' in text_line):
             size_array = []
             if (text_line.count('[')>1):
@@ -309,7 +309,7 @@ def stan_read(text_line_raw,v_type,v_size,v_name,v_bounds,v_num):
         v_bounds.append(bounds)
         v_num += 1
     elif ('cholesky_factor' in text_line):
-	v_type.append('cholesky_factor')
+        v_type.append('cholesky_factor')
         if ('[' in text_line):
             size_array = []
             if (text_line.count('[')>1):
@@ -392,39 +392,39 @@ def get_operator(string):
             if (return_ind == -1):
                 return_ind = ind
                 return_operator = operator[n]
-		k = ind + 1
-		flag_end = False
-		# check for operator-bracket combinations
-		while (k < len(string)):
-		    for m in range(0,6):
-			if (string[k] == operator[m]):
-			    return_operator += operator[m]
-			    return_ind = k
-			    flag_end = True
-		    if (flag_end == True):
-			break
-		    elif (string[k] != ' '):
-			break
-		    else:
-		        k += 1
+                k = ind + 1
+                flag_end = False
+                # check for operator-bracket combinations
+                while (k < len(string)):
+                    for m in range(0,6):
+                        if (string[k] == operator[m]):
+                            return_operator += operator[m]
+                            return_ind = k
+                            flag_end = True
+                    if (flag_end == True):
+                        break
+                    elif (string[k] != ' '):
+                        break
+                    else:
+                        k += 1
             elif (ind < return_ind):
                 return_ind = ind
                 return_operator = operator[n]
-		k = ind + 1
-		flag_end = False
-		# check for operator-bracket combinations
-		while (k < len(string)):
-		    for m in range(0,6):
-			if (string[k] == operator[m]):
-			    return_operator += operator[m]
-			    return_ind = k
-			    flag_end = True
-		    if (flag_end == True):
-			break
-		    elif (string[k] != ' '):
-			break
-		    else:
-		        k += 1
+                k = ind + 1
+                flag_end = False
+                # check for operator-bracket combinations
+                while (k < len(string)):
+                    for m in range(0,6):
+                        if (string[k] == operator[m]):
+                            return_operator += operator[m]
+                            return_ind = k
+                            flag_end = True
+                    if (flag_end == True):
+                        break
+                    elif (string[k] != ' '):
+                        break
+                    else:
+                        k += 1
     return return_ind,return_operator
 
 # Gets the size string of a parameter in a format which can be evaluated
@@ -445,7 +445,7 @@ def get_eval_string(size_string,data_name,data_value):
             [next_ind,operator] = get_operator(size_string[last_ind:])
             if (next_ind == -1):
                 name = remove_whitespace(size_string[last_ind:],False)
-		last_ind = len(size_string)
+                last_ind = len(size_string)
                 if (is_number(name) == True):
                     eval_string += name
                 else:
@@ -471,198 +471,198 @@ def get_eval_string(size_string,data_name,data_value):
 # main function
 def make_outfile(stan_file, data_file, author):
 
-	print(stan_file)
+    print(stan_file)
 
-	model_fname = stan_file[stan_file.rfind('/')+1:-5]
-	data_fname = stan_file[data_file.rfind('/')+1:-5]
+    model_fname = stan_file[stan_file.rfind('/')+1:-5]
+    data_fname = stan_file[data_file.rfind('/')+1:-5]
 
-	f_stan = open(stan_file, 'r')
-	stan_text = f_stan.readlines()
+    f_stan = open(stan_file, 'r')
+    stan_text = f_stan.readlines()
 
-	# Parse stan file
-	flag_data=False
-	flag_parameters=False
-	flag_model=False
-	data_num=0
-	param_num=0
-	model_num=0
-	param_type=[]
-	data_type=[]
-	param_name=[]
-	data_name=[]
-	param_size=[]
-	data_size=[]
-	param_bounds=[]
-	data_bounds=[]
-	v_mu=[]
-	v_sigma=[]
-	v_bounds=[]
-	calc_order=[]
-	for text_line in stan_text:
-	    # highlight relevant sections
-	    if ('data {' in text_line):
-		if ('transformed' not in text_line):
-			flag_data = True
-	    elif ('parameters {' in text_line):
-		if ('transformed' not in text_line):
-		    flag_parameters = True
-	    elif ('model {' in text_line):
-		flag_model = True
-	    # handle each section
-	    elif (flag_data == True):
-		if ('}' in text_line):
-		    flag_data = False
-		else:
-		    [data_type,data_size,data_name,data_bounds,data_num]=stan_read(text_line,data_type,data_size,data_name,data_bounds,data_num)
-	    elif (flag_parameters == True):
-		if ('}' in text_line):
-		    flag_parameters = False
-		else:
-		    [param_type,param_size,param_name,param_bounds,param_num]=stan_read(text_line,param_type,param_size,param_name,param_bounds,param_num)
-		    
-	f_stan.close()
+    # Parse stan file
+    flag_data=False
+    flag_parameters=False
+    flag_model=False
+    data_num=0
+    param_num=0
+    model_num=0
+    param_type=[]
+    data_type=[]
+    param_name=[]
+    data_name=[]
+    param_size=[]
+    data_size=[]
+    param_bounds=[]
+    data_bounds=[]
+    v_mu=[]
+    v_sigma=[]
+    v_bounds=[]
+    calc_order=[]
+    for text_line in stan_text:
+        # highlight relevant sections
+        if ('data {' in text_line):
+            if ('transformed' not in text_line):
+                flag_data = True
+        elif ('parameters {' in text_line):
+            if ('transformed' not in text_line):
+                flag_parameters = True
+        elif ('model {' in text_line):
+            flag_model = True
+        # handle each section
+        elif (flag_data == True):
+            if ('}' in text_line):
+                flag_data = False
+            else:
+                [data_type,data_size,data_name,data_bounds,data_num]=stan_read(text_line,data_type,data_size,data_name,data_bounds,data_num)
+        elif (flag_parameters == True):
+            if ('}' in text_line):
+                flag_parameters = False
+            else:
+                [param_type,param_size,param_name,param_bounds,param_num]=stan_read(text_line,param_type,param_size,param_name,param_bounds,param_num)
+            
+    f_stan.close()
 
 
-	f_data = open(data_file, 'r')
-	data_text = f_data.readlines()
+    f_data = open(data_file, 'r')
+    data_text = f_data.readlines()
 
-	flag_array = False
-	flag_start = False
-	data_value = {}
+    flag_array = False
+    flag_start = False
+    data_value = {}
 
-	# Parse data file
-	for text_line in data_text:
-	    if ('<-' in text_line):
-		if (flag_array == True):
-		    flag_array = False
-		    data_value[index] = np.array(robjects.reval(array_string))
-		elif (flag_start == True):
-		    Exception('Variable has unassigned value in data file')
-		name_end = text_line.find('<')
-		name = remove_whitespace(text_line[0:name_end],False)
-		index = try_find(name,data_name)
-		if (index != -1):
-		    if (data_size[index] == 1):
-			if ('c(' in text_line):
-			    raise Exception('Specified data sizes do not match those in the data file')
-			else:
-			    value_start = text_line.rfind('-') + 1
-			    value = remove_whitespace(text_line[value_start:],False)
-			    if (is_number(value) == True):
-				if (value[-1:] == '\n'):
-				    data_value[index] = value[:-1]
-				else:
-				    data_value[index] = value
-			    else:
-				flag_start = True
-		    else:
-			if ('c(' in text_line):
-			    flag_array = True
-			    value_start = text_line.find('-') + 1
-			    if (text_line[-2:] == '\n'):
-				array_string = text_line[value_start:-3]
-			    else:
-				array_string = text_line[value_start:]
-			else:
-			    flag_start = True
-	    elif (flag_array == True):
-		if (text_line[-2:] == '\n'):
-		    array_string += text_line[:-3]
-		else:
-		    array_string += text_line
-	    elif (flag_start == True):
-		if (data_size[index] == 1):
-		    if ('c(' in text_line):
-			raise Exception('Specified data sizes do not match those in the data file')
-		    else:
-			if (text_line[-1:] == '\n'):
-			    value = remove_whitespace(text_line[:-1],False)
-			else:
-			    value = remove_whitespace(text_line,False)
-			if (is_number(value) == True):
-			    data_value[index] = value
-			    flag_start = False
-		else:
-		    if ('c(' in text_line):
-			flag_array = True
-			#value_start = text_line.rfind('c')
-			#array_string = text_line[value_start:]
-			if (text_line[-2:] == '\n'):
-			    array_string = text_line[:-3]
-			else:
-			    array_string = text_line
-			flag_start = False
+    # Parse data file
+    for text_line in data_text:
+        if ('<-' in text_line):
+            if (flag_array == True):
+                flag_array = False
+                data_value[index] = np.array(robjects.reval(array_string))
+            elif (flag_start == True):
+                Exception('Variable has unassigned value in data file')
+            name_end = text_line.find('<')
+            name = remove_whitespace(text_line[0:name_end],False)
+            index = try_find(name,data_name)
+            if (index != -1):
+                if (data_size[index] == 1):
+                    if ('c(' in text_line):
+                        raise Exception('Specified data sizes do not match those in the data file')
+                    else:
+                        value_start = text_line.rfind('-') + 1
+                        value = remove_whitespace(text_line[value_start:],False)
+                        if (is_number(value) == True):
+                            if (value[-1:] == '\n'):
+                                data_value[index] = value[:-1]
+                            else:
+                                data_value[index] = value
+                        else:
+                            flag_start = True
+                else:
+                    if ('c(' in text_line):
+                        flag_array = True
+                        value_start = text_line.find('-') + 1
+                        if (text_line[-2:] == '\n'):
+                            array_string = text_line[value_start:-3]
+                        else:
+                            array_string = text_line[value_start:]
+                    else:
+                        flag_start = True
+        elif (flag_array == True):
+            if (text_line[-2:] == '\n'):
+                array_string += text_line[:-3]
+            else:
+                array_string += text_line
+        elif (flag_start == True):
+            if (data_size[index] == 1):
+                if ('c(' in text_line):
+                    raise Exception('Specified data sizes do not match those in the data file')
+                else:
+                    if (text_line[-1:] == '\n'):
+                        value = remove_whitespace(text_line[:-1],False)
+                    else:
+                        value = remove_whitespace(text_line,False)
+                    if (is_number(value) == True):
+                        data_value[index] = value
+                        flag_start = False
+        else:
+            if ('c(' in text_line):
+                flag_array = True
+                #value_start = text_line.rfind('c')
+                #array_string = text_line[value_start:]
+                if (text_line[-2:] == '\n'):
+                    array_string = text_line[:-3]
+                else:
+                    array_string = text_line
+                flag_start = False
 
-	if (flag_array == True):
-		data_value[index] = np.array(robjects.reval(array_string))
-		
-	f_data.close()
+    if (flag_array == True):
+        data_value[index] = np.array(robjects.reval(array_string))
+        
+    f_data.close()
 
-	if len(data_value) != len(data_name):
-	    raise Exception('Specified data sizes do not match those in the data file')
+    #if len(data_value) != len(data_name):
+    #    raise Exception('Specified data sizes do not match those in the data file')
 
-	eval_param_size=[]
+    eval_param_size=[]
 
-	if (param_num>0):
-	    for size_string in param_size:
-		if (isinstance(size_string, int)):
-		    eval_param_size.append(size_string)
-		elif (isinstance(size_string, str)):
-		    # not list, only one element to deal with
-		    eval_param_size.append(eval(get_eval_string(size_string,data_name,data_value)))
-		else:
-		    eval_param_size.append(1)
-		    for size_string_element in size_string:
-			# evaluate elements and multiply together to get total size
-			#print(get_eval_string(size_string_element,data_name,data_value))
-			eval_param_size[-1] *= eval(get_eval_string(size_string_element,data_name,data_value))
+    if (param_num>0):
+        for size_string in param_size:
+            if (isinstance(size_string, int)):
+                eval_param_size.append(size_string)
+            elif (isinstance(size_string, str)):
+                # not list, only one element to deal with
+                eval_param_size.append(eval(get_eval_string(size_string,data_name,data_value)))
+            else:
+                eval_param_size.append(1)
+                for size_string_element in size_string:
+                    # evaluate elements and multiply together to get total size
+                    #print(get_eval_string(size_string_element,data_name,data_value))
+                    eval_param_size[-1] *= eval(get_eval_string(size_string_element,data_name,data_value))
 
-	# get keywords from directory
-	index = -1
-	ind = 0
-	keywords = []
-	url = 'https://github.com/stan-dev/example-models'
-	while ind < len(stan_file)-5:
-	    if (stan_file[ind] == '/'):
-		ind += 1
-		index += 1
-		if (ind < len(stan_file)-5):
-		    keywords.append(stan_file[ind])
-		    url += '/' + stan_file[ind]
-		    ind += 1
-		else:
-		    url += '/'
-		    break
-	    elif (ind == 0):
-		index += 1
-		keywords.append(stan_file[ind])
-		url += '/' + stan_file[ind]
-		ind += 1
-	    else:
-		keywords[index] += stan_file[ind]
-		url += stan_file[ind]
-		ind += 1
+    # get keywords from directory
+    index = -1
+    ind = 0
+    keywords = []
+    url = 'https://github.com/stan-dev/example-models'
+    while ind < len(stan_file)-5:
+        if (stan_file[ind] == '/'):
+            ind += 1
+            index += 1
+            if (ind < len(stan_file)-5):
+                keywords.append(stan_file[ind])
+                url += '/' + stan_file[ind]
+                ind += 1
+            else:
+                url += '/'
+                break
+        elif (ind == 0):
+            index += 1
+            keywords.append(stan_file[ind])
+            url += '/' + stan_file[ind]
+            ind += 1
+        else:
+            keywords[index] += stan_file[ind]
+            url += stan_file[ind]
+            ind += 1
 
-	outf = open("{}.json".format(model_fname), "w")
+    outf = open("{}.json".format(model_fname), "w")
 
-	# write .json file
-	outf.write('{\n')
-	outf.write('  "name": "{}",\n'.format(model_fname))
-	outf.write('  "keywords": {},\n'.format(json.dumps(keywords)))
-	outf.write('  "urls": "{}",\n'.format(url))
-	outf.write('  "model_name": "{}",\n'.format(model_fname))
-	outf.write('  "data_name": "{}",\n'.format(data_fname))
-	outf.write('  "reference_posterior_name": null,\n')
-	outf.write('  "references": [],\n')
-	outf.write('  "dimensions": {\n')
-	if (len(param_name) > 1):
-	    for n in range(0, len(param_name)-1):
-		outf.write('    "{}": {},\n'.format(param_name[n],eval_param_size[n]))
-	    outf.write('    "{}": {}\n'.format(param_name[-1],eval_param_size[-1]))
-	else:
-	    outf.write('    "{}": {}\n'.format(param_name[0],eval_param_size[0]))
-	outf.write('  },\n')
-	outf.write('  "added_date": "{}",\n'.format(date.today()))
-	outf.write('  "added_by": "{}"\n'.format(author))
-	outf.write('}')
+    # write .json file
+    outf.write('{\n')
+    outf.write('  "name": "{}",\n'.format(model_fname))
+    outf.write('  "keywords": {},\n'.format(json.dumps(keywords)))
+    outf.write('  "urls": "{}",\n'.format(url))
+    outf.write('  "model_name": "{}",\n'.format(model_fname))
+    outf.write('  "data_name": "{}",\n'.format(data_fname))
+    outf.write('  "reference_posterior_name": null,\n')
+    outf.write('  "references": [],\n')
+    outf.write('  "dimensions": {\n')
+    if (len(param_name) > 1):
+        for n in range(0, len(param_name)-1):
+            outf.write('    "{}": {},\n'.format(param_name[n],eval_param_size[n]))
+        outf.write('    "{}": {}\n'.format(param_name[-1],eval_param_size[-1]))
+    else:
+        outf.write('    "{}": {}\n'.format(param_name[0],eval_param_size[0]))
+    outf.write('  },\n')
+    outf.write('  "added_date": "{}",\n'.format(date.today()))
+    outf.write('  "added_by": "{}"\n'.format(author))
+    outf.write('}')
 
